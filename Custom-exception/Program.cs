474c0905439017d5f34125a0f4315256e495ecd2
RizvanVeliyev <veliyevrizvan250@gmail.com>
2024-06-27 19:23:40 +0400

    Add project files.

---
+++ Custom-exception/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Custom_exception
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
